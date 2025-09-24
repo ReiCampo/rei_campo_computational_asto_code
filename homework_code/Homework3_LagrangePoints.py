5da8ca7678aca6f8f24c62a363d3a90c1d5db064
Rei Campo <rachel.campo@rutgers.edu>
2025-09-24 18:22:46 -0400

    Adding in almost-finalized homework. Newtonian method is not currently working.

--- homework_code/Homework3_LagrangePoints.py
+++ homework_code/Homework3_LagrangePoints.py
@@ -14,13 +14,12 @@
 # Starting by importing necessary packages:
 
 import numpy as np
-from astropy import constants as const
 from astropy import units as u
-import matplotlib.pyplot as plt
+import argparse
 
 # Creating the function that will solve the equation numerically:
 
-def lagrange_points(start_value, numerical_evaluation, units = "SI"):
+def lagrange_points(start_value, numerical_evaluation, units = "SI", error_tolerance = 1e-5):
     '''
     This function will solve for the distance the Earth-Moon Lagrange Point 1 is
     from the center of the Earth. 
@@ -42,10 +41,17 @@ def lagrange_points(start_value, numerical_evaluation, units = "SI"):
             list of acceptable inputs are:
             "SI"
             "AU"
+            "Kilometers"
+            "Centimeters"
+            "Milimeters"
+
+        error_tolerance (float):
+            The allowable error tolerance the numerical calculation of the non-
+            linear equation. It is automatically set to 1e-10
     
 
     Outputs:
-        distance_to_L1 (float):
+        lagrange_point_distance (str):
             This is the distance from the center of the Earth to the Lagrange
             Point 1 in the units specified by the user.
 
@@ -80,12 +86,14 @@ def lagrange_points(start_value, numerical_evaluation, units = "SI"):
 
         units = units.upper()
 
-        if units not in ("SI", "ERG"):
-            raise ValueError("Your 'units' input is not a value that is"
+        if units not in ("SI", "AU", "KILOMETERS", "CENTIMETERS", "MILIMETERS"):
+            raise ValueError("Your 'units' input is not a value that is "
             "recognized by the function. You inputted: " + str(units) + ".  "
-            "Please enter one of the acceptable values:\n"
-            "'SI'\n"
-            "'erg'")
+            "Please enter one of the acceptable values: \n"
+            "'Meters' \n"
+            "'AU' \n"
+            "'Centimeters' \n"
+            "")
 
     elif isinstance(units, str) == False:
         raise ValueError("The 'units' input requires a string input. You have " \
@@ -136,12 +144,12 @@ def lagrange_points(start_value, numerical_evaluation, units = "SI"):
         h_value = float(input("You have selected the Newtonian method to numerically " \
             "solve for the Lagrangian Point. Please type in an (h) value to " \
             "calculate the derivative using the central difference method. A " \
-            "recommended (h) value is 1e-5."))
+            "recommended (h) value is 1e-5. "))
 
         # Using the central difference method to find the derivative for the
         # function:
 
-        def newtonian_derivative(h): ### Need to fix this later
+        def newtonian_derivative(h): 
 
             df = (lagrange_equation(start_value + (h/2)) - lagrange_equation(start_value - (h/2))) / h
 
@@ -150,21 +158,18 @@ def lagrange_points(start_value, numerical_evaluation, units = "SI"):
         # Creating an initializer to be used in the while loop:
 
         newtonian_initializer = 0
+        
+        newton_guess = start_value
 
         # Setting up the while loop:
 
         while newtonian_initializer < 1000:
 
-            newton_guess = start_value
-
             # Calculating the guess:
 
             newton_calculation = newton_guess - (lagrange_equation(newton_guess) / newtonian_derivative(h_value))
-
-            # I chose 1e-10 arbitrarily since that the error of the root being
-            # 1e-10 off the true value is allowable:
              
-            if abs(newton_calculation) < 1e-10:
+            if abs( (newton_calculation - newton_guess) / newton_calculation) < error_tolerance:
                 # Update the distance_to_L1 variable to the calculated value and
                 # break from the loop: 
                 distance_to_L1 = newton_calculation
@@ -180,36 +185,133 @@ def lagrange_points(start_value, numerical_evaluation, units = "SI"):
 
     elif numerical_evaluation == "SECANT":
 
-        second_value = float(input("You have selected the Secant method to" \
-            "numerically solve for the Lagrangian Point. To use this method," \
-            "the calculation needs two starting values. Your input for" \
+        # User is prompted to input a second value so the secant method can be
+        # calculated: 
+
+        second_value = float(input("You have selected the Secant method to " \
+            "numerically solve for the Lagrangian Point. To use this method, " \
+            "the calculation needs two starting values. Your input for " \
             "start_value will be used as the first point, however you must " \
-            "choose another point. It is recommended to select a second value" \
-            "that is relativley close to your start_value."))
+            "choose another point. It is recommended to select a second value " \
+            "that is relativley close to your start_value. Your start_value " \
+            "input was: " + str(start_value) + ". "))
+        
+        # Creating the function that will be used to calculate the third point:
 
-        def secant_point_value(second_point):
+        def secant_point_value(first_point, second_point):
 
-            x3 = second_point - lagrange_equation(second_point) * ((second_point - start_value) / (lagrange_equation(second_point) - lagrange_equation(start_value)))
+            x3 = second_point - lagrange_equation(second_point) * ((second_point - first_point) / (lagrange_equation(second_point) - lagrange_equation(first_point)))
 
             return x3
         
+        # Creating an initializer to be used later in the while loop:
+        
         secant_initializer = 0
+
+        # Creating starting values that will be updated later by the while
+        # loop: 
+
+        secant_guess1 = start_value 
+
+        secant_guess2 = second_value
+
+        # Setting up the while loop:
 
         while secant_initializer < 1000:
 
-            secant_guess1 = start_value 
-
-            secant_guess2 = second_value
+            # Calculating the third point:
 
             secant_calculation = secant_point_value(secant_guess1, secant_guess2)
 
-            if abs(secant_calculation) < 1e-10:
+            # I chose 1e-10 arbitrarily since that the error of the root being
+            # 1e-10 off the true value is allowable:
+
+            if abs( (secant_calculation - secant_guess2) / secant_calculation) < error_tolerance:
+                # If the value is small enough, distance_to_L1 will be updated
+                # to the secant_calculation and the while loop will be broken 
+                # out of: 
                 distance_to_L1 = secant_calculation
                 break
+
             else:
+                # Loop will continue the calculation and update the guesses and
+                # the initializer:
                 secant_guess1 = secant_guess2
                 secant_guess2 = secant_calculation
                 secant_initializer = secant_initializer + 1
 
+    # Converting the distances to what the user inputted:
+
+    distance_to_L1 = distance_to_L1 * u.meter
+
+    if units == "AU":
+        distance_to_L1 = format(distance_to_L1.to(u.au), ".6")
+    elif units == "KILOMETERS":
+        distance_to_L1 = format(distance_to_L1.to(u.km), ".6e")
+    elif units == "CENTIMETERS":
+        distance_to_L1 = format(distance_to_L1.to(u.cm), ".6e")
+    elif units == "MILIMETERS":
+        distance_to_L1 = format(distance_to_L1.to(u.mm), ".6e")
+    else:
+        distance_to_L1 = format(distance_to_L1, ".6e")
+
     
-    return distance_to_L1
+    
+    lagrange_point_distance = "The approximate distance of the Earth-Moon " \
+    "Lagrange point 1 using the " + str(numerical_evaluation) + " method is: " \
+    + str(distance_to_L1)
+
+    return lagrange_point_distance
+
+
+
+# Creating parser so the code can be called on the command line:
+
+parser = argparse.ArgumentParser(description = "Calculate the Earth-Moon " \
+"Lagrangian Point 1 using either the Newtonian or Secant numerical evaluation " \
+"method for non-linear equations.")
+
+# Creating the start parser
+parser.add_argument("start_value", 
+                    type = float, 
+                    help = "This is the initial guess value that will be used " \
+                    "in both the Newtonian and Secant methods.")
+
+# Creating the end parser
+parser.add_argument("numerical_evaluation", 
+                    type = str,
+                    help = "This is the numerical calculation method that will " \
+                    "be used to solve the non-linear equation that determines " \
+                    "the distance of the Earth-Moon Lagrange Point 1. The " \
+                    "acceptable values are: \n" \
+                    "'Newtonian' \n" \
+                    "'Secant'")
+
+# Creating the steps parser
+parser.add_argument("--units",
+                    default = "SI",
+                    type = str,
+                    help = "The units of the distance to from the center of the " \
+                    "Earth to the L1 point. If no units are specifie, SI are " \
+                    "automatically used, however acceptable inputs are: \n" \
+                    "'AU' \n" \
+                    "'Kilometers' \n" \
+                    "'Centimeters' \n" \
+                    "'Milimeters'")
+
+# Creating the method parser
+parser.add_argument("--error_tolerance",
+                    default = 1e-10,
+                    type = float,
+                    help = "The amount of tolerable error the numerical " \
+                    "calculation can be off when finding the root of the " \
+                    "non-linear equation. It is automatically set to 1e-10.")
+
+args = parser.parse_args()
+
+L1_distance = lagrange_points(args.start_value,
+                              args.numerical_evaluation,
+                              args.units,
+                              args.error_tolerance)
+
+print(L1_distance)
